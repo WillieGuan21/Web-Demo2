f7ab6be0c1de2dfa1af2224c141dcf44f6e3aa7c
willie <willie@wsos.com.tw>
2021-09-15 09:26:02 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+ function formReset(){
+   document.getElementById("textarea").reset();
+ }
